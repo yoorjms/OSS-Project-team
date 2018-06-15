5fbe98f9989aa64a94442bbd4e1920eac2266610
정지범 <2377325@gmail.com>
2018-06-15 12:26:02 +0900

    빅사이즈 쇼핑몰 스크래피
    Signed-off-by: 정지범 <2377325@gmail.com>

---
+++ bigsize mall shopping scrapy/javascript scrap.py
@@ -0,0 +1,30 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+chrome_path = r"D:\선희\3학년\오픈소스 SW\chromedriver_win32\chromedriver.exe"
+
+# driver = webdriver.Chrome(chrome_path)
+# driver.get("http://vancouver.craigslist.com")
+# driver.find_element_by_xpath("""//*[@id="sss0"]/li[22]/a""").click()
+
+driver = webdriver.Chrome(chrome_path)
+driver.get("http://www.bigsizeclub.co.kr/")
+driver.find_element_by_xpath("""//*[@id="topmenu_first"]/div/div[2]/form/fieldset/a/i""").click()
+#river.find_element_by_xpath("""//*[@id="cate_top"]/a""").click()
+element = driver.find_element_by_name("content1")
+driver.execute_script("arguments[0].value = '티';",element)
+driver.find_element_by_xpath("""//*[@id="search_form"]/fieldset/div/table/tbody/tr[1]/td[3]/div/a/img""").click()
+
+posts = driver.find_elements_by_class_name("MS_prod_img_s")
+
+for post in posts :
+    print(post.get_attribute('src'))
+
+nametemp = post.get(' ')
+
+
+
+# posts = driver.find_elements_by_class_name("MS_prod_img_s")
+# print(posts)
+#//*[@id="footer"]/div[2]/div/div[3]/div/div[1]/ul/li[2]/a
+# for post in posts:
+#     print(post.text)
